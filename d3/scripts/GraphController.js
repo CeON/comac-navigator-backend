99ac706f48ec761d0ac2de1d9b88ba4e371127ce
Michał Oniszczuk <michal.oniszczuk@gmail.com>
2014-12-16 16:34:46 +0100

    Refactoring of GraphController.

--- d3/scripts/GraphController.js
+++ d3/scripts/GraphController.js
@@ -15,24 +15,24 @@ function GraphController(dataProvider) {
 
   this.force = d3.layout.force()
     .charge(-600)
-    //.charge(function(node) {
-    //  if (node.favourite)
-    //    return -500;;
-    //  else
-    //    return -1600;
-    //})
     .linkDistance(120)
-    //.linkDistance(function(link) {
-    //  if (link.favourite)
-    //    return 100;
-    //  else
-    //    return 150;
-    //})
-    .size([this.width, this.height]);
+    .size([this.width, this.height])
+    .on("tick", (function() {
+      this.graphView.paths
+        .attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+      this.graphView.circles
+        .attr("transform", function(d, i) {
+            return "translate(" + [ d.x,d.y ] + ")"
+        });
+    }).bind(this));
 
   this.graphModel =
-    { nodes       : null
-    , links       : null
+    { nodes       : []
+    , links       : []
     , favouriteIds: ["paper_2", "paper_3", "author_1"]
     }
 
@@ -40,10 +40,8 @@ function GraphController(dataProvider) {
 
   dataProvider.getGraphByFavouriteIds(
       this.graphModel.favouriteIds,
-      this.showInitialGraph.bind(this));
+      this.updateGraph.bind(this));
 
-  //this.graphModel.nodes.map(function(d) { d.favourite = false; return d; });
-  //setTimeout(this.restart.bind(this), 1000);
   setTimeout((function() {this.addFavouriteNodes(["paper_0"])}).bind(this), 1000);
 }
 
@@ -52,72 +50,19 @@ GraphController.prototype = {
   width:  960,
   height: 500,
 
-  showInitialGraph: function(error, graph) {
-    //console.log(JSON.stringify(graph))
-
-    this.graphModel.nodes = graph.nodes;
-    this.graphModel.links = GraphController.nodeIdsToReferences(
-                                                  this.graphModel.nodes,
-                                                  graph.links);
-
-    //console.log(this.graphModel.links[0]);
-    //console.log(JSON.stringify(this.graphModel.nodes));
-    //console.log(JSON.stringify(this.graphModel.links));
-
-    this.force
-      .nodes(this.graphModel.nodes)
-      .links(this.graphModel.links)
-      .on("tick", (function() {
-        this.graphView.paths
-          .attr("x1", function(d) { return d.source.x; })
-          .attr("y1", function(d) { return d.source.y; })
-          .attr("x2", function(d) { return d.target.x; })
-          .attr("y2", function(d) { return d.target.y; });
-
-        this.graphView.circles
-          .attr("transform", function(d, i) {
-              return "translate(" + [ d.x,d.y ] + ")"
-          });
-      }).bind(this));
-
-    this.restart();
-    this.showNodesFirefoxHack();
-  },
-
-  addFavouriteNodes: function(newNodeIds) {
-    this.graphModel.favouriteIds =
-      this.graphModel.favouriteIds.concat(newNodeIds);
-    this.dataProvider.getGraphByFavouriteIds(
-        this.graphModel.favouriteIds, 
-        this.showChangedGraph.bind(this));
-
-    // this.graphModel.addFavouriteNodes(newNodeIds);
-    // this.restart();
-  },
-
-  showChangedGraph: function(error, graph) {
+  updateGraph: function(error, graphJSON) {
     var oldNodes = this.graphModel.nodes;
-
-    this.graphModel.nodes = graph.nodes;
-    this.graphModel.links = GraphController.nodeIdsToReferences(
-                                                  this.graphModel.nodes,
-                                                  graph.links);
-    GraphController.repositionNodes(oldNodes, graph.nodes);
+    GraphModel.updateGraphModel(this.graphModel, graphJSON);
+    GraphModel.repositionNodes(oldNodes, this.graphModel.nodes);
 
     this.force
       .nodes(this.graphModel.nodes)
       .links(this.graphModel.links)
-    this.restart();
-    this.showNodesFirefoxHack();
+      .start();
+    this.updateGraphView();
   },
 
-  showNodesFirefoxHack: function() {
-    n = d3.selectAll(".node")
-    n.classed("node", false)
-    setTimeout(function(){n.classed("node", true)}, 0);
-  },
-
-  restart: function() {
+  updateGraphView: function() {
     // paths (links)
     this.graphView.paths = this.graphView.paths
       .data(this.graphModel.links)
@@ -185,7 +130,21 @@ GraphController.prototype = {
     // remove old nodes
     this.graphView.circles.exit().remove();
 
-    this.force.start();
+    this.showNodesFirefoxHack();
+  },
+
+  showNodesFirefoxHack: function() {
+    n = d3.selectAll(".node")
+    n.classed("node", false)
+    setTimeout(function(){n.classed("node", true)}, 0);
+  },
+
+  addFavouriteNodes: function(newNodeIds) {
+    this.graphModel.favouriteIds =
+      this.graphModel.favouriteIds.concat(newNodeIds);
+    this.dataProvider.getGraphByFavouriteIds(
+        this.graphModel.favouriteIds, 
+        this.updateGraph.bind(this));
   }
 }
 
@@ -197,20 +156,36 @@ GraphController.shortenString = function(str) {
   }
 }
 
-GraphController.nodeIdsToReferences = function(nodes, links) {
-  return links.map(function(link) {
-    link.source = nodes.filter(function(node) {
-      return node.id === link.sourceId;
-    })[0];;
-    link.target = nodes.filter(function(node) {
-      return node.id === link.targetId;
-    })[0];
-    link.hover = false;
-    return link;
-  });
+
+/**
+ * GraphModel class
+ *
+ * @author Michał Oniszczuk michal.oniszczuk@gmail.com
+ */
+
+
+function GraphModel() {}
+
+GraphModel.updateGraphModel = function(graphModel, graphJSON) {
+  graphModel.nodes = graphJSON.nodes;
+  graphModel.links = nodeIdsToReferences(graphJSON.nodes,
+                                         graphJSON.links);
+
+  function nodeIdsToReferences(nodes, links) {
+    return links.map(function(link) {
+      link.source = nodes.filter(function(node) {
+        return node.id === link.sourceId;
+      })[0];
+      link.target = nodes.filter(function(node) {
+        return node.id === link.targetId;
+      })[0];
+      link.hover = false;
+      return link;
+    });
+  }
 }
 
-GraphController.repositionNodes = function(oldNodesRaw, newNodes) {
+GraphModel.repositionNodes = function(oldNodesRaw, newNodes) {
   var oldNodes = asDictionaryByIds(oldNodesRaw);
   newNodes.forEach(repositionSingleNode(oldNodes));
 
